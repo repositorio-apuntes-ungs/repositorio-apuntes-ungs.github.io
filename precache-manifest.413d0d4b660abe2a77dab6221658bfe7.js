0a7954c86746263503fc3c05629968387ce9222b
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2020-03-19 14:55:42 -0300

    First commit

---
+++ precache-manifest.413d0d4b660abe2a77dab6221658bfe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b3f44aa831a02e969c6011d5378f9f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d577b728d44006296cdf",
+    "url": "./static/css/main.f012d565.chunk.css"
+  },
+  {
+    "revision": "ad24a292c759a7a265fe",
+    "url": "./static/js/2.34ccebef.chunk.js"
+  },
+  {
+    "revision": "ba1665840a4211d22917503a2a5544f6",
+    "url": "./static/js/2.34ccebef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d577b728d44006296cdf",
+    "url": "./static/js/main.2a8742c5.chunk.js"
+  },
+  {
+    "revision": "7b9601fc4643fe492ab3",
+    "url": "./static/js/runtime-main.30a1386b.js"
+  },
+  {
+    "revision": "b95448b9cd1a17cca110646331e88fb7",
+    "url": "./static/media/ubyd.b95448b9.jpg"
+  }
+]);
